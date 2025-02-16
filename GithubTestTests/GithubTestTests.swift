d186011e30634db340341449051460419779cfb0
adampiotrowiczdev <34211589+adampiotrowiczdev@users.noreply.github.com>
2025-02-16 23:36:29 +0100

    Initial Commit

---
+++ GithubTestTests/GithubTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubTestTests.swift
+//  GithubTestTests
+//
+//  Created by adam on 16/02/2025.
+//
+
+import Testing
+@testable import GithubTest
+
+struct GithubTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
